ed055e52a798a8fe4b6c8ec7bf5da53b6f234ff3
Yashasvi Chaudhary <yashasvichaudhry98@gmail.com>
2024-03-04 17:30:24 +0530

    added removeSpecialCharacters method to trim revert reasons

--- src/gas-estimator/entry-point-v6/utils/index.ts
+++ src/gas-estimator/entry-point-v6/utils/index.ts
@@ -115,6 +115,7 @@ export function getVerificationGasEstimationSimulatorResult(
 }
 
 export function handleFailedOp(revertReason: string) {
+  revertReason = removeSpecialCharacters(revertReason);
   if (revertReason.includes("AA1") || revertReason.includes("AA2")) {
     throw new RpcError(
       revertReason,
@@ -145,6 +146,25 @@ export function handleFailedOp(revertReason: string) {
     "UserOperation reverted during execution phase",
     VALIDATION_ERRORS.SIMULATE_VALIDATION_FAILED,
   );
+}
+
+function removeSpecialCharacters(input: string): string {
+  const match = input.match(/AA(\d+)\s(.+)/);
+
+  if (match) {
+    const errorCode = match[1]; // e.g., "25"
+    const errorMessage = match[2]; // e.g., "invalid account nonce"
+    const newMatch = `AA${errorCode} ${errorMessage}`.match(
+      // eslint-disable-next-line no-control-regex
+      /AA.*?(?=\\u|\u0000)/,
+    );
+    if (newMatch) {
+      const extractedString = newMatch[0];
+      return extractedString;
+    }
+    return `AA${errorCode} ${errorMessage}`;
+  }
+  return input;
 }
 
 export function getSimulationResult(
