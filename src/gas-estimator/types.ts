672039ab788825155dffc4d952b03f8171ad7282
fichiokaku <filip.dujmusic@biconomy.io>
2025-03-06 16:34:21 +0400

    fix types

--- src/gas-estimator/types.ts
+++ src/gas-estimator/types.ts
@@ -63,8 +63,6 @@ export interface IEntryPointV6Simulations extends IEntryPointV6 {
   estimateVerificationGasLimit: typeof EntryPointV6Simulations.prototype.estimateVerificationGasLimit
   /** Estimates call gas limit */
   estimateCallGasLimit: typeof EntryPointV6Simulations.prototype.estimateCallGasLimit
-  /** Estimates call gas limit with markers*/
-  estimateCallGasLimitWithMarkers: typeof EntryPointV6Simulations.prototype.estimateCallGasLimitWithMarkers
 }
 
 /**
